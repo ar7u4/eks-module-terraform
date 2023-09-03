3d30d58a52ae124693ded10b4c8c33f6edf855cd
arjun <arjun.betageri@live.com>
2023-09-03 13:35:07 +0530

    terraform EKS modules

--- main.tf
+++ main.tf
@@ -1,50 +1,118 @@
+# Configure the AWS provider
 provider "aws" {
   region = var.region
 }
 
-# Create VPC
-module "vpc" {
-  source      = "terraform-aws-modules/vpc/aws"
-  name        = "eks-vpc"
-  cidr        = var.vpc_cidr
-  azs         = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = var.private_subnet_cidrs
-  public_subnets  = var.public_subnet_cidrs
+# Create a VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
 }
 
-# Create EKS Cluster
-module "eks_cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.21"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-  tags            = { "Environment" = "Dev" }
+# Create subnets (public and private)
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = element(["a", "b", "c"], count.index)
+  map_public_ip_on_launch = true
 }
 
-# Create EKS Node Group
-module "eks_node_group" {
-  source          = "terraform-aws-modules/eks/aws//modules/node_group"
-  cluster_name    = module.eks_cluster.cluster_id
-  node_group_name = "example-node-group"
-  node_role_name  = "eks_node_role"
-  node_instance_type   = var.eks_node_group_instance_type
-  desired_capacity     = var.eks_node_group_desired_capacity
-  min_size             = var.eks_node_group_min_size
-  max_size             = var.eks_node_group_max_size
-  subnet_ids           = module.vpc.private_subnets
-  tags = {
-    "Environment" = "Dev"
-    "Name"        = "example-node-group"
+resource "aws_subnet" "private_subnet" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = element(["a", "b", "c"], count.index)
+  map_public_ip_on_launch = false
+}
+
+# Create a security group for EKS nodes
+resource "aws_security_group" "eks_node_sg" {
+  name        = "eks-node-sg"
+  description = "Security group for EKS nodes"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  # Add your security group rules here as needed
+}
+
+# Create the EKS cluster
+resource "aws_eks_cluster" "example" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.private_subnet[*].id
   }
+}
+
+# Create an IAM role for the EKS cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach necessary policies to the IAM role for EKS cluster (e.g., AmazonEKSClusterPolicy)
+resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  roles      = [aws_iam_role.eks_cluster_role.name]
+}
+
+# Create a node group for the EKS cluster
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  scaling_config {
+    desired_size = var.eks_node_group_desired_capacity
+    max_size     = var.eks_node_group_max_size
+    min_size     = var.eks_node_group_min_size
+  }
+
+  subnet_ids = aws_subnet.private_subnet[*].id
+}
+
+# Create an IAM role for EKS nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 # Output the EKS Cluster Configuration
 output "eks_cluster_config" {
-  value = module.eks_cluster
+  value = aws_eks_cluster.example
 }
 
 # Output the EKS Node Group Configuration
 output "eks_node_group_config" {
-  value = module.eks_node_group
+  value = aws_eks_node_group.example
+}
+
+# Output the VPC ID
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.eks_vpc.id
 }
--- output.tf
+++ output.tf
@@ -1,14 +1,12 @@
+output "eks_cluster_config" {
+  value = aws_eks_cluster.example
+}
+
+output "eks_node_group_config" {
+  value = aws_eks_node_group.example
+}
+
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-output "eks_cluster_id" {
-  description = "ID of the EKS cluster"
-  value       = module.eks_cluster.cluster_id
-}
-
-output "eks_node_group_id" {
-  description = "ID of the EKS node group"
-  value       = module.eks_node_group.node_group_id
+  value       = aws_vpc.eks_vpc.id
 }
